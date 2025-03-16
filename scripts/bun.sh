02c28be634b3b4caa387dbf0ab1d0ddffa51d407
Juan C. Yamacho H <jcyamacho@gmail.com>
2025-03-16 12:24:31 +0100

    feat: add scripts for bun, deno, and nvm installations to enhance JavaScript runtime management

---
+++ scripts/bun.sh
@@ -0,0 +1,10 @@
+# BUN (javascript runtime): https://bun.sh/
+export BUN_DIR="$HOME/.bun"
+if [ ! -d $BUN_DIR ]; then
+    info "Installing bun..."
+    curl -fsSL https://bun.sh/install | sh
+fi
+
+[ -s "$BUN_DIR/_bun" ] && source "$BUN_DIR/_bun"
+
+export PATH="$BUN_DIR/bin:$PATH"
--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -4,6 +4,7 @@ if [ ! -d $NVM_DIR ]; then
     info "Installing nvm..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | sh
 fi
+
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
@@ -20,20 +21,3 @@ function update-nodejs() {
         nvm uninstall "$version"
     done
 }
-
-# BUN (javascript runtime): https://bun.sh/
-export BUN_DIR="$HOME/.bun"
-if [ ! -d $BUN_DIR ]; then
-    info "Installing bun..."
-    curl -fsSL https://bun.sh/install | sh
-fi
-[ -s "$BUN_DIR/_bun" ] && source "$BUN_DIR/_bun"
-export PATH="$BUN_DIR/bin:$PATH"
-
-# DENO (javascript runtime): https://deno.land/
-export DENO_INSTALL="$HOME/.deno"
-if [ ! -d $DENO_INSTALL ]; then
-    info "Installing deno..."
-    curl -fsSL https://deno.land/install.sh | sh -s -- --no-modify-path -y
-fi
-export PATH="$DENO_INSTALL/bin:$PATH"
