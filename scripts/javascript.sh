99c24de45f77504e1704e5d851f6e1908c13d2f6
Juan C. Yamacho H <jcyamacho@gmail.com>
2025-03-16 12:17:12 +0100

    feat: add update-nodejs function to automate Node.js and npm updates

--- scripts/javascript.sh
+++ scripts/javascript.sh
@@ -7,6 +7,20 @@ fi
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+function update-nodejs() {
+    info "Updating Node.js..."
+    nvm install --lts
+    nvm use --lts --default
+
+    info "Updating npm..."
+    npm install -g npm@latest
+
+    info "Removing old Node.js versions..."
+    nvm ls | grep -v $(nvm current) | grep -o "v[0-9]\+\.[0-9]\+\.[0-9]\+" | while read -r version; do
+        nvm uninstall "$version"
+    done
+}
+
 # BUN (javascript runtime): https://bun.sh/
 export BUN_DIR="$HOME/.bun"
 if [ ! -d $BUN_DIR ]; then
