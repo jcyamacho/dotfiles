6e981cc11b9b04c21e0876aa501f4314b5d97815
Juan C. Yamacho H <jcyamacho@gmail.com>
2025-05-27 16:21:56 +0200

    chore: update fabric-ai.mac.sh to change installation check from fabric to fabric-ai and adjust patterns directory path

--- scripts/fabric-ai.mac.sh
+++ scripts/fabric-ai.mac.sh
@@ -1,22 +1,27 @@
 # FABRIC_AI (AI prompts framework): https://github.com/danielmiessler/fabric
-if ! exists fabric; then
-  info "Installing fabric..."
-  brew install fabric
+if ! exists fabric-ai; then
+  info "Installing fabric-ai..."
+  brew install fabric-ai
 fi
 
 alias fabric="fabric-ai"
 
-# Loop through all files in the ~/.config/fabric/patterns directory
-for pattern_file in $HOME/.config/fabric/patterns/*; do
-    # Get the base name of the file (i.e., remove the directory path)
-    pattern_name=$(basename "$pattern_file")
+export FABRIC_AI_DIR="$HOME/.config/fabric-ai"
+export FABRIC_AI_PATTERNS_DIR="$FABRIC_AI_DIR/patterns"
 
-    # Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
-    alias_command="alias $pattern_name='fabric --pattern $pattern_name'"
+if [ -d $FABRIC_AI_DIR ]; then
+  # Loop through all files in the ~/.config/fabric-ai/patterns directory
+  for pattern_file in $FABRIC_AI_PATTERNS_DIR/*; do
+      # Get the base name of the file (i.e., remove the directory path)
+      pattern_name=$(basename "$pattern_file")
 
-    # Evaluate the alias command to add it to the current shell
-    eval "$alias_command"
-done
+      # Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
+      alias_command="alias $pattern_name='fabric --pattern $pattern_name'"
+
+      # Evaluate the alias command to add it to the current shell
+      eval "$alias_command"
+  done
+fi
 
 yt() {
     if [ "$#" -eq 0 ] || [ "$#" -gt 2 ]; then
