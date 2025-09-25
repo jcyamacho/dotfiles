49a0b305f04936e856dd8dd6f67f7099f32719fd
Juan C. Yamacho H. <jcyamacho@gmail.com>
2025-09-25 21:25:49 +0200

    chore: refactor zsh config to use antidote use-omz plugin

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,24 +1,19 @@
 # sets the cursor to a blinking vertical bar (beam) style
-echo -ne '\e[5 q'
+echo -ne "\e[5 q"
+
 export DEFAULT_EDITOR="zed"
 
+############################## UPDATES ##############################
 updates=()
 
-########################## OMZ COMPAT ##########################
-export ZSH_CACHE_DIR="${ZSHDOTFILES_DIR}/.cache/zsh"
-if [[ ! -d "${ZSH_CACHE_DIR}" ]]; then
-  mkdir -p "${ZSH_CACHE_DIR}"
-fi
+update-all() {
+  for update in "${updates[@]}"; do
+    $update
+    echo
+  done
 
-export ZSH_COMPLETIONS_DIR="${ZSH_CACHE_DIR}/completions"
-if [[ ! -d "${ZSH_COMPLETIONS_DIR}" ]]; then
-  mkdir -p "${ZSH_COMPLETIONS_DIR}"
-fi
-
-fpath=("${ZSH_COMPLETIONS_DIR}" $fpath)
-
-autoload -Uz compinit
-compinit
+  reload
+}
 
 ########################## CUSTOM_TOOLS_DIR ##########################
 export CUSTOM_TOOLS_DIR="$HOME/.local/bin"
@@ -78,12 +73,12 @@ fpath=("${ANTIDOTE_DIR}/functions" $fpath)
 autoload -Uz antidote
 
 # Generate a new static file whenever .zsh_plugins.txt is updated.
-if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
-  antidote bundle <${zsh_plugins}.txt >|${zsh_plugins}.zsh
+if [[ ! "${zsh_plugins}.zsh" -nt "${zsh_plugins}.txt" ]]; then
+  antidote bundle < "${zsh_plugins}.txt" >| "${zsh_plugins}.zsh"
 fi
 
 # Source your static plugins file.
-source ${zsh_plugins}.zsh
+source "${zsh_plugins}.zsh"
 
 _update_antidote() {
   info "Updating antidote..."
@@ -183,13 +178,3 @@ fi
 for script in "$ZSHDOTFILES_DIR/scripts"/*.sh; do
   source "$script"
 done
-
-############################## UPDATES ##############################
-update-all() {
-  for update in "${updates[@]}"; do
-    $update
-    echo
-  done
-
-  reload
-}
