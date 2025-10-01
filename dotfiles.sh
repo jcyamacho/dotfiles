b6c6a7277fa6ae613908d252f0d1bb72ee1a887a
Juan C. Yamacho H. <jcyamacho@gmail.com>
2025-10-01 16:32:08 +0200

    chore: set terminal title to current working directory

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,7 @@
 # sets the cursor to a blinking vertical bar (beam) style
 echo -ne "\e[5 q"
+# sets the terminal title to the current working directory
+echo -ne "\033]0;${pwd}\007"
 
 export DEFAULT_EDITOR="zed"
 
